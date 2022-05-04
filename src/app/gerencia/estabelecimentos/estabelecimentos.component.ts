53bcb4cab882f81cf2f34b723f4b47b78315af6e
Bruno Ribeiro <vilhenab8gmail.com>
2022-05-04 10:58:44 -0300

    Finalizando o botão bloquear

--- src/app/gerencia/estabelecimentos/estabelecimentos.component.ts
+++ src/app/gerencia/estabelecimentos/estabelecimentos.component.ts
@@ -1,65 +1,45 @@
-
 import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
-import { Component, OnInit, NgModule, Input, SimpleChange } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  NgModule,
+  Input,
+  SimpleChange,
+} from '@angular/core';
 
 @Component({
   selector: 'app-estabelecimentos',
   templateUrl: './estabelecimentos.component.html',
-  styleUrls: ['./estabelecimentos.component.scss']
+  styleUrls: ['./estabelecimentos.component.scss'],
 })
 export class EstabelecimentosComponent implements OnInit {
-
-  estabelecimentos=[
+  estabelecimentos = [
     {
-      nome:"",
-      status:"",
-      formasPagamento:[],
-      slogan:"",
-      valor:"",
-      _id:""
+      nome: '',
+      status: '',
+      formasPagamento: [],
+      slogan: '',
+      valor: '',
+      _id: '',
+    },
+  ];
+  estados = [{ estado: false, id:'' }];
+  bloquear() {
+    for (let i = 0; i < this.estados.length; i++) {
+      console.log(' Bloquear:', this.estados);
     }
-  ]
- estado: boolean = false;
- id:string = "";
- bloquear(estado: boolean,id: string){
-   estado = this.estado;
-   id = this.id
-  console.log(estado,id);
- }
-
- checkboxChange(event:any){
-  this.estado=event.target.checked
-  this.id=(event.target.id);
-  this.bloquear(this.estado,this.id)
-
-
- }
-
-
-
-  constructor(
-    private authService: AutenticacaoService) {
-    }
-
+  }
+  checkboxChange(event: any) {
+    let estado = event.target.checked;
+    let id = event.target.id;
+    if(estado==true)
+    this.estados.push({ estado: estado, id: id });
+  }
+  constructor(private authService: AutenticacaoService) {}
   ngOnInit(): void {
     this.authService.getEstabelecimentos().subscribe((estabelecimentos) => {
       console.log(estabelecimentos);
-
-
       this.estabelecimentos = estabelecimentos;
-
-
-
-    })
-
-
-
-
-
+    });
   }
-
-
-
-
-
 }
