9835179577fb89a4298a3f2050fa63809b43da3b
Bruno Ribeiro <vilhenab8gmail.com>
2022-05-12 16:11:14 -0300

    tela agendamento e menu

--- src/app/gerencia/agendamento/agendamento.component.ts
+++ src/app/gerencia/agendamento/agendamento.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
-=======
->>>>>>> e7190aaa2f16cf892ace09631d9294c3a4d02cbd
 
 @Component({
   selector: 'app-agendamento',
@@ -10,7 +7,6 @@ import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
   styleUrls: ['./agendamento.component.scss']
 })
 export class AgendamentoComponent implements OnInit {
-<<<<<<< HEAD
   user!: string;
   estabelecimentos=[
     {
@@ -34,12 +30,6 @@ export class AgendamentoComponent implements OnInit {
 
     });
     this.user = this.authService.user;
-=======
-
-  constructor() { }
-
-  ngOnInit(): void {
->>>>>>> e7190aaa2f16cf892ace09631d9294c3a4d02cbd
   }
 
 }
