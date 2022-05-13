8339a708543f99377e3e0e49abf6a28d5f551882
bruno-vribeiro <vilhenab8@gmail.com>
2022-05-13 11:30:24 -0300

    trabalhando com bloquear

--- src/app/gerencia/cliente/cliente.component.ts
+++ src/app/gerencia/cliente/cliente.component.ts
@@ -4,32 +4,37 @@ import { Component, OnInit, NgModule } from '@angular/core';
 @Component({
   selector: 'app-cliente',
   templateUrl: './cliente.component.html',
-  styleUrls: ['./cliente.component.scss']
+  styleUrls: ['./cliente.component.scss'],
 })
 export class ClienteComponent implements OnInit {
   user!: string;
-  estabelecimentos=[
+  estabelecimentos = [
     {
-      nome:"",
-      status:"",
-      formasPagamento:[],
-      slogan:"",
-      valor:""
+      nome: '',
+      status: '',
+      formasPagamento: [],
+      slogan: '',
+      valor: '',
+    },
+  ];
+  estados: any = [];
+  status_m(num: any) {
+    if (num == 0) {
+      this.estados.push('bloquear');
+    } else {
+      this.estados.push('liberar');
     }
-  ]
+  }
 
-  constructor(
-    private authService: AutenticacaoService
-  ) { }
+  constructor(private authService: AutenticacaoService) {}
 
   ngOnInit(): void {
     this.authService.getEstabelecimentos().subscribe((estabelecimentos) => {
-      console.log(estabelecimentos);
       this.estabelecimentos = estabelecimentos;
-
-
+      for (let i = 0; i < estabelecimentos.length; i++) {
+        this.status_m(estabelecimentos[i].status);
+      }
     });
     this.user = this.authService.user;
   }
-
 }
